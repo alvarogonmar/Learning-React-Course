ecabb83649cf926eccf199a04aa24967e39e1d76
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-04 22:47:12 -0600

    Adding let

--- Introduction_to_JavaScript/js/01.js
+++ Introduction_to_JavaScript/js/01.js
@@ -1,2 +1,15 @@
 // PRINT 
-console.log('Hola Mundo')
+console.log('Hello world')
+
+// VARIABLES
+let customer = 'Alvaro'
+
+//REASSING VARIABLE
+customer = 30
+//PRINTING
+console.log(customer)
+
+//2
+let price
+price=1000
+console.log(price)
