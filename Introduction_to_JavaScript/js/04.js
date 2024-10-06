60f00a1ee6391d820a642a3a42ebf1129268937d
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-05 23:36:06 -0600

    Adding objects information and console.table

--- Introduction_to_JavaScript/js/04.js
+++ Introduction_to_JavaScript/js/04.js
@@ -1,1 +1,10 @@
-// Objects
+// Objects (key and value)
+const product1 = {
+    name : 'TV',
+    price : 300
+}
+console.log(product1)
+console.log(typeof product1)
+
+//PRINT WITH TABLE
+console.table(product1)
