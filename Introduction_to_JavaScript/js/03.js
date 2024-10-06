d680f3302bdc74937c15af86722cc3193215f3a6
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-05 23:16:29 -0600

    Adding Strings

--- Introduction_to_JavaScript/js/03.js
+++ Introduction_to_JavaScript/js/03.js
@@ -4,3 +4,12 @@
 let cliente
 //Printing the type
 console.log(typeof cliente)
+
+//Strings
+const student = 'Alvaro'
+console.log(student)
+console.log(typeof student)
+
+let product = 'iPhone'
+console.log(product)
+console.log(typeof product)
