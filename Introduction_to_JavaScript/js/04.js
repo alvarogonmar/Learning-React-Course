5f84a56f001bd34b7b4dc1279f27db8d37492c78
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-06 17:56:36 -0600

    Adding DESTRUCTURING

--- Introduction_to_JavaScript/js/04.js
+++ Introduction_to_JavaScript/js/04.js
@@ -1,7 +1,8 @@
 // Objects (key and value)
 const product1 = {
     name : 'TV',
-    price : 300
+    price : 300,
+    stock : false
 }
 console.log(product1)
 console.log(typeof product1)
@@ -13,3 +14,16 @@ console.table(product1)
 console.log(product1.name)
 console.log(product1.price)
 
+//DESTRUCTURING
+const { name, price, stock } = product1
+console.log(name)
+console.log(price)
+console.log(stock)
+
+//This does the same as the code above but is longer.
+//const name = product1.name
+//const price = product1.price
+//const stock = product1.stock
+//console.log(name)
+//console.log(price)
+//console.log(stock)
