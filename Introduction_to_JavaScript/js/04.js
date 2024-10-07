4654a98cdcdc2af63e1e890f198cb02f2ff45a3b
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-06 18:14:20 -0600

    Adding OBJECT LITERAL ENHACEMENT

--- Introduction_to_JavaScript/js/04.js
+++ Introduction_to_JavaScript/js/04.js
@@ -17,3 +17,14 @@ console.log(product1.price2)
 const { name1, price2 } = product1
 console.log(name1)
 console.log(price2)
+
+// OBJECT LITERAL ENHACEMENT (when you have an operation, you have to return a lot of variables, so this helps to return only one object)
+const authenticated = true
+const user = 'Alvaro'
+
+const newObject = {
+    authenticated,
+    //another way
+    user: user
+}
+console.log(newObject)
