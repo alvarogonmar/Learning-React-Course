0fc3dfdaa93492bf5a650e221bde86bb9d9b9e63
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-06 18:22:27 -0600

    adding a extra value and delete a value

--- Introduction_to_JavaScript/js/05.js
+++ Introduction_to_JavaScript/js/05.js
@@ -8,5 +8,9 @@ const product1 = {
 
 // rewrite a value
 product1.stock = true
+// Adding a value if it doesnt exist
+product1.image = 'imagen.jpg'
+// Delete a value
+delete product1.price2
 
 console.log(product1)
