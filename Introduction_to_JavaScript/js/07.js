6851a02c702f30be4982d72f8362d51056e822ee
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-06 18:52:22 -0600

    Adding 07.js

---
+++ Introduction_to_JavaScript/js/07.js
@@ -0,0 +1,18 @@
+// Objects - Destructuring 2 or more objects
+
+const product1 = {
+    name1 : 'TV',
+    price2 : 300,
+    stock : false
+}
+
+const customer1 = {
+    name1 : 'Alvaro',
+    premium : true
+}
+
+const shopping_car = {
+    amount : 1,
+    product1
+}
+console.log(shopping_car)
