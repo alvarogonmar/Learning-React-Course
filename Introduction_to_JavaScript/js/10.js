ac36547b6f64de35f054cea568ea8b9de1687bf0
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-06 19:38:38 -0600

    Adding a new value with push

--- Introduction_to_JavaScript/js/10.js
+++ Introduction_to_JavaScript/js/10.js
@@ -1,3 +1,14 @@
 const technologies = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js']
+// REPLACE A VALUE THAT ALREADY EXIST
+// technologies[4] = 'Nest.js'
+// ADDING A VALUE
+// technologies[5] = 'Nest.js'
 
+// ADD A VALUE AT THE END
+// technologies.push('Nest.js')
+
+// ADD A VALUE AT THE END (BETTER TO USE WITH REACT)
+const newArray = [...technologies, 'Nest.js']
 console.table(technologies)
+
+console.table(newArray)
