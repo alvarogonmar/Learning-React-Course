eb7065f53ddec6fb3de088243622132f69fb5576
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-06 19:19:05 -0600

    Using Backticks

--- Introduction_to_JavaScript/js/08.js
+++ Introduction_to_JavaScript/js/08.js
@@ -3,3 +3,9 @@
 const product = '12in Tablet'
 const price = 400
 const brand = 'Orange'
+
+// Concatenation
+console.log('The product is: ' + product)
+// THE SAME AS ABOVE BUT BETTER, WITH BACKTICKS
+console.log(`The product is: ${product}`)
+console.log(`${product} $${price} Dollars, brand: ${brand}`)
