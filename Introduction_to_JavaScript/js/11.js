86439d30af648becf248709695b243f98b5c20bc
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-10 22:22:14 -0600

    Using destructuring for arrays

--- Introduction_to_JavaScript/js/11.js
+++ Introduction_to_JavaScript/js/11.js
@@ -1,1 +1,11 @@
 const technologies = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js']
+
+const react = technologies[3]
+// DESTRUCTURING ARRAYS
+// EXTRACT THE POSITION AND PRINT IT
+const [html, css, nodejs] = technologies
+
+// FOR EXAMPLE HERE I HAVE CSS, POSITION [1], and PRINT 'CSS' position [1] of THE ARRAY
+console.log(css)
+// IF I HAD nodejs IN POSITION [2], ITS gonna PRINT 'JavaScript' BECAUSE IS IN THE POSITION [2]
+console.log(nodejs)
