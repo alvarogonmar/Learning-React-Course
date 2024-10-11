0badf155f715a50e9949e36d5deea5febda12542
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-10 22:27:51 -0600

    another way to print react.js

--- Introduction_to_JavaScript/js/11.js
+++ Introduction_to_JavaScript/js/11.js
@@ -7,5 +7,14 @@ const [html, css, nodejs] = technologies
 
 // FOR EXAMPLE HERE I HAVE CSS, POSITION [1], and PRINT 'CSS' position [1] of THE ARRAY
 console.log(css)
+
 // IF I HAD nodejs IN POSITION [2], ITS gonna PRINT 'JavaScript' BECAUSE IS IN THE POSITION [2]
 console.log(nodejs)
+
+// IF I WANT to PRINT ONLY 'React.js':
+const [ , , , reactjs] = technologies // USING COMMAS IN THE BLANK SPACE (THERE WOULD BE THE VARIABLE)
+console.log(reactjs)
+
+// but this is correct too:
+const react2 = technologies[3]
+console.log(react2)
