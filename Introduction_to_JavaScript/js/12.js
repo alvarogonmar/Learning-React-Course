14bece8f8d6b91acab97edccdc7ccf9e8391d9a5
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-11 22:39:03 -0600

    Adding Foreach

--- Introduction_to_JavaScript/js/12.js
+++ Introduction_to_JavaScript/js/12.js
@@ -2,6 +2,9 @@ const technologies = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js']
 
 
 // PRINT ALL THE ELEMENTS IN THE ARRAY, USING LENGTH TO KNOW HOW MANY ELEMENTS ARE IN THE ARRAY.
-for(let i = 0; i < technologies.length; i ++) {
-    console.log(technologies[i])
-}
+// for(let i = 0; i < technologies.length; i ++) {
+//     console.log(technologies[i])
+// }
+
+// Foreach : knows how many elements are in the array and executes those times
+
