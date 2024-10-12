a236996e517aa4a232a0d26c4417f56d980675d2
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-11 22:35:10 -0600

    12.JS FINISHED

--- Introduction_to_JavaScript/js/12.js
+++ Introduction_to_JavaScript/js/12.js
@@ -1,1 +1,7 @@
 const technologies = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js']
+
+
+// PRINT ALL THE ELEMENTS IN THE ARRAY, USING LENGTH TO KNOW HOW MANY ELEMENTS ARE IN THE ARRAY.
+for(let i = 0; i < technologies.length; i ++) {
+    console.log(technologies[i])
+}
