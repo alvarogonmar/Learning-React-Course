a67d1365b79a3164c0d482b25a24f44e4312c8c0
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-11 21:57:53 -0600

    adding sum2 with default numbers

--- Introduction_to_JavaScript/js/13.js
+++ Introduction_to_JavaScript/js/13.js
@@ -1,7 +1,15 @@
 // Function Declaration
 
-function sum(x,y) {
+function sum(x,y) { // Parametros
     console.log( x + y )
 }
 
-sum(10,20)
+sum(10,20) // Argumentos
+
+function sum2(num1 = 0, num2 = 0){
+    console.log(num1)
+    console.log(num2)
+    console.log(num1+num2)
+}
+
+sum2()
