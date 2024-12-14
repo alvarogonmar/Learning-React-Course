58477e1bc7b3406cee65ece402994b5063529e9a
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-13 20:17:25 -0600

    Export defaults modules

--- Introduction_to_JavaScript/js/24.js
+++ Introduction_to_JavaScript/js/24.js
@@ -1,6 +1,11 @@
-import multipl, { sum } from "./functions.js"
+import multipl, { division, res, sum } from "./functions.js"
 
-const result1 =  sum(100, 20)
+const result1 =  sum(20, 10)
+const result2 = res(20,10)
+const result3 = multipl(20,10)
+const result4 = division(20,10)
+
 console.log(result1)
-const result2 = multipl(20,10)
 console.log(result2)
+console.log(result3)
+console.log(result4)
--- Introduction_to_JavaScript/js/functions.js
+++ Introduction_to_JavaScript/js/functions.js
@@ -1,19 +1,30 @@
-export function sum(n1, n2){
-    return n1 + n2
-}
-// agregar el export, para permitirle importar esas funciones en otros archivos
-export function res(n1, n2){
-    return n1 - n2
-}
-
-// o tambien puede ser:
-// export {
-//     sum,
-//     res
+// export function sum(n1, n2){
+//     return n1 + n2
+// }
+// // agregar el export, para permitirle importar esas funciones en otros archivos
+// export function res(n1, n2){
+//     return n1 - n2
 // }
 
+// // o tambien puede ser:
+// // export {
+// //     sum,
+// //     res
+// // }
 
-// Export default: solo puede haber uno por archivo, puedes nombrarlo como quieras y no debes ponerlo entre las llaves{}
-export default function mult(n1, n2){
-    return n1*n2
-}
+
+// // Export default: solo puede haber uno por archivo, puedes nombrarlo como quieras y no debes ponerlo entre las llaves{}
+// export default function mult(n1, n2){
+//     return n1*n2
+// }
+
+// Converit el codigo a arrow functions:
+ export const sum = (n1, n2) => n1 + n2
+ 
+ export const res = (n1, n2) => n1 - n2
+ 
+const mult = (n1, n2) => n1*n2
+
+export const division = (n1,n2) => n1/n2
+
+export default mult
