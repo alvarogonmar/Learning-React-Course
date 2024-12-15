2d3e52b43b3630407cc2081e903c01eb0a6ddac0
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-14 22:19:29 -0600

    DOM - Clicks

--- Introduction_to_JavaScript/js/30.js
+++ Introduction_to_JavaScript/js/30.js
@@ -1,1 +1,14 @@
 // Eventos del DOM - Clicks
+const heading = document.querySelector(".heading")
+const links = document.querySelectorAll(".navigation a")
+
+// heading.addEventListener("click", () => {
+//     heading.textContent = "New Heading Click"
+// })
+
+links.forEach( link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault()
+        e.target.textContent = "Diste click..."
+    })
+})
