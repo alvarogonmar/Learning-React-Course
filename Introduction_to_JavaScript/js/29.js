89c0c5d83a11d8313d95f08d7f09a5886d547a8e
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-14 20:33:13 -0600

    HTML manipulation with JS

--- Introduction_to_JavaScript/js/29.js
+++ Introduction_to_JavaScript/js/29.js
@@ -1,5 +1,13 @@
-// Selectores
+// Manipular elementos HTML con JS
 
 const heading = document.querySelector(".heading")
 const links = document.querySelectorAll(".navigation a")
-console.log(links)
+heading.textContent = "New Heading" // Ponerle un nuevo heading
+
+heading.removeAttribute("class") // Quitarle el atributo 
+
+// const inputName = document.querySelector("#name")
+// inputName.value = "New Name..."
+
+// Al usar querySelectorAll tengo que ponerle la posicion [i] para poder manipularlos
+links[0].textContent = "New Link"
