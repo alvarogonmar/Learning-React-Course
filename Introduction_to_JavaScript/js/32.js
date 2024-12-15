5d35b696c662e49920256195e4d0e3024d1ff92d
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-14 23:13:40 -0600

    Showing an alert on the website

--- Introduction_to_JavaScript/js/32.js
+++ Introduction_to_JavaScript/js/32.js
@@ -7,12 +7,29 @@ form.addEventListener("submit", e => {
     const name = document.querySelector("#name").value
     const password = document.querySelector("#password").value
 
+    // Prevenir nuevas alertas
+    const advanceAlert = document.querySelector(".alert")
+    advanceAlert?.remove()
+
+    const alert = document.createElement("DIV") // creatElement sirve para generar elementos HTML con codigo de JS
+    alert.textContent = "Alert Content"
+    alert.classList.add("alert", "text-white", "uppercase", "text-sm", "text-center", "p-2", "font-black") // Ponerle classes al div
+    
 
     if(name === "" || password === ""){
-        console.log("All fields are required")
+        alert.textContent = "All the fields are required"
+        alert.classList.add("bg-red-500")
     } else {
-        console.log("Login...")
+        alert.textContent = "Login..."
+        alert.classList.add("bg-green-500")
+
     }
+
+    form.appendChild(alert) // Agregarselo como hijo del formulario
+
+    setTimeout(() => { // Borrarla de la pagina despues de 2s
+        alert.remove()
+    }, 2000);
     // console.log(name.value)
     // console.log(password.value)
     // console.log("Sending form...")
